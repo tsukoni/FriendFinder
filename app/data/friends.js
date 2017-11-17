beb631be5c29cf4654555d670af966c3c329e6df
unknown <vuvuong8@aol.com>
2017-11-16 22:26:09 -0800

    layout set

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+		name: 'Anakin Skywalker',
+		photo: '',
+		scores: [1, 5, 1, 1, 5, 5, 5, 1, 5, 5]
+	},{
+		name: 'Jack Sparrow',
+		photo: '',
+		scores: [4, 1, 4, 5, 1, 1, 1, 5, 1, 1]
+	},{
+		name: 'Phil Dunphy',
+		photo: '',
+		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},{
+		name: 'Natalie Coughlin',
+		photo: '',
+		scores: [5, 1, 5, 2, 1, 1, 1, 5, 1, 1]
+	}];
+
+module.exports = friends;
