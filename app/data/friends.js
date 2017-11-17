4264a1a1f015aecf36e899dcc967c4d43f0c88ae
unknown <vuvuong8@aol.com>
2017-11-17 07:27:33 -0800

    deployment attempt

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,24 @@
 var friends = [
 	{
 		name: 'Anakin Skywalker',
-		photo: '',
-		scores: [1, 5, 1, 1, 5, 5, 5, 1, 5, 5]
+		photo: 'https://vignette4.wikia.nocookie.net/the-universal-experiment/images/e/ef/Rots32.jpg/revision/latest?cb=20130522110521',
+		scores: [10, 3, 3, 2, 10, 10, 5, 8, 1, 10]
 	},{
 		name: 'Jack Sparrow',
-		photo: '',
-		scores: [4, 1, 4, 5, 1, 1, 1, 5, 1, 1]
+		photo: 'http://media.beliefnet.com/~/media/photos-with-attribution/entertainment/movies/jack-sparrow_credit-walt-disney-studios.jpg',
+		scores: [8, 10, 6, 8, 5, 5, 8, 8, 10, 8]
 	},{
-		name: 'Phil Dunphy',
-		photo: '',
-		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+		name: 'Catherine Tramell',
+		photo: 'https://i.pinimg.com/originals/56/e1/d1/56e1d133ffb75a74f0f89631588f6719.jpg',
+		scores: [10, 8, 9, 10, 3, 10, 1, 3, 5, 10]
 	},{
-		name: 'Natalie Coughlin',
-		photo: '',
-		scores: [5, 1, 5, 2, 1, 1, 1, 5, 1, 1]
+		name: 'Lara Croft',
+		photo: 'https://entropymag.org/wp-content/uploads/2017/09/rise-of-the-tomb-raider.jpg',
+		scores: [5, 6, 9, 10, 5, 8, 10, 10, 9, 3]
+	},{
+		name: 'Hermione Granger',
+		photo: 'http://vignette1.wikia.nocookie.net/p__/images/5/5e/Hermione-granger.jpg/revision/latest?cb=20160713085217&path-prefix=protagonist',
+		scores: [3, 4, 10, 10, 7, 4, 10, 7, 3, 1]
 	}];
 
 module.exports = friends;
